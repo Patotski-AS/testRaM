36a7ed795256c657b9538e44f8b8329868dc45bb
Andrey Patotski <7906241@gmail.com>
2022-04-14 19:41:27 +0300

    feat: inject CharacterInfoViewModel

--- app/src/main/java/com/example/android/testram/presentation/ui/character_info/CharacterInfoViewModel.kt
+++ app/src/main/java/com/example/android/testram/presentation/ui/character_info/CharacterInfoViewModel.kt
@@ -1,7 +1,10 @@
 package com.example.android.testram.presentation.ui.character_info
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class CharacterInfoViewModel : ViewModel() {
+@HiltViewModel
+class CharacterInfoViewModel @Inject constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }
