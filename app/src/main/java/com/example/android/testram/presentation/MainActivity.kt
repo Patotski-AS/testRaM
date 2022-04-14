239b74b02de04c5cf1904cac14c43ea2fcc0dc5a
Andrey Patotski <7906241@gmail.com>
2022-04-14 19:19:41 +0300

    feat: add nav_graph

--- app/src/main/java/com/example/android/testram/presentation/MainActivity.kt
+++ app/src/main/java/com/example/android/testram/presentation/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.android.testram.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import com.example.android.testram.R
 import com.example.android.testram.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +11,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
