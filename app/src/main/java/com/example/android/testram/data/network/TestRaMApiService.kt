1439f1e6d9f36aeae4112b7e320a86af6f0f5c14
Andrey Patotski <7906241@gmail.com>
2022-04-17 22:15:06 +0300

    refactor: TestRaMApiService.kt

--- app/src/main/java/com/example/android/testram/data/network/TestRaMApiService.kt
+++ app/src/main/java/com/example/android/testram/data/network/TestRaMApiService.kt
@@ -1,15 +1,23 @@
 package com.example.android.testram.data.network;
 
+import com.example.android.testram.data.network.pojo.CharacterPojo
 import com.example.android.testram.data.network.pojo.CharacterResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 private const val PARAMS_PAGE = "page"
 
 interface TestRaMApiService {
 
     @GET("/api/character")
-    suspend fun getCharacters(
-        @retrofit2.http.Query(PARAMS_PAGE) page: String
-    ): Response<CharacterResponse>
+    suspend fun getListCharacters(
+        @retrofit2.http.Query(PARAMS_PAGE) page: Int
+    ):Response<CharacterResponse?>
+
+    @GET("/api/character/{id}")
+    suspend fun getCharacter(
+        @Path("id") id: String
+    ):Response<CharacterPojo?>
+
 }
